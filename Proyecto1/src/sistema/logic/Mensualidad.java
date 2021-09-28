0a7aaa4903a9f2b9ae62ae272abc1f07d8e02f5d
nelsonporras1 <80367737+nelsonporras1@users.noreply.github.com>
2021-09-27 20:55:33 -0600

    mensualidad prestamo
    prestamo y mensaulidad se pueden modificar para que den numeros redondeados

--- Proyecto1/src/sistema/logic/Mensualidad.java
+++ Proyecto1/src/sistema/logic/Mensualidad.java
@@ -10,5 +10,54 @@ package sistema.logic;
  * @author 50689
  */
 public class Mensualidad {
+    private double numero;
+    private double saldo;
+    private double interes;
+    private double amortizacion;
     
-}
+    
+    public Mensualidad( double numero,double saldo,double interes,double amortizacion){
+        this.numero=numero;
+        this.saldo=saldo;
+        this.interes=interes;
+        this.amortizacion=amortizacion;
+    }
+    
+    public void setNumero( double numero){
+        this.numero=numero;
+    };
+    
+     public void setSaldo( double saldo){
+        this.saldo=saldo;
+    };
+     
+      public void setInteres( double interes){
+        this.interes=interes;
+    };
+    
+       public void setAmortizacion( double amortizacion){
+        this.amortizacion=amortizacion;
+    };
+    
+        public double getNumero(){
+            return numero;
+        };
+
+        public double getSaldo(){
+            return saldo;
+        };
+
+        public double getInteres(){
+            return interes;
+        };
+
+        public double getAmortizacion(){
+        return amortizacion;
+        };
+    
+       public String toString(){
+    return " numero :" + getNumero()+"\n"+" Saldo :" + getSaldo()+"\n"+" monto interes :" + getInteres()+"\n"+" amortizacion: " + getAmortizacion()+"\n";
+   
+    
+       }
+       }
--- Proyecto1/src/sistema/logic/Prestamo.java
+++ Proyecto1/src/sistema/logic/Prestamo.java
@@ -5,10 +5,89 @@
  */
 package sistema.logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author 50689
  */
 public class Prestamo {
+
+ private double monto;
+ private double plazo;
+ private double tasaInteres;
+
+    public Prestamo(double monto, double plazo, double tasaInteres) {
+        this.monto = monto;
+        this.plazo = plazo;
+        this.tasaInteres = tasaInteres;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public void setPlazo(double plazo) {
+        this.plazo = plazo;
+    }
+
+    public void setTasaInteres(double tasaInteres) {
+        this.tasaInteres = tasaInteres;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public double getPlazo() {
+        return plazo;
+    }
+
+    public double getTasaInteres() {
+        return tasaInteres;
+    }
+ 
+
+    
+    public double calculaCuota(){
+       double num1=monto*tasaInteres;
+       double base=(1+tasaInteres);
+       double exponente=plazo*-1;
+       double num2= 1-(Math.pow(base,exponente));
+       double cuota= num1/num2;
+    return cuota;
+    }
+    
+    public String llenarMensualidades(){
+        double saldoActual =monto;
+        int num=1;
+        double mI=saldoActual*getTasaInteres();
+        double mA=calculaCuota()-mI;
+        List<Mensualidad> mensualidades= new ArrayList<>();
+        
+        for(int i=1;i<= getPlazo();i++){
+            Mensualidad m =new Mensualidad(num,saldoActual,mI,mA);
+            mensualidades.add(m);
+            saldoActual=m.getSaldo()-m.getAmortizacion();
+           mI=saldoActual*getTasaInteres();
+           mA=calculaCuota()-mI;
+            num++;
+    
+    }
+        return mensualidades.toString();
+    }
+    
+    
+    
+    
+    
+    
+    
+    @Override
+    public String toString(){
+    return "monto :" + getMonto()+"\n"+"plazo :" + getPlazo()+"\n"+"tasa interes :" + getTasaInteres()+"\n"+"Cuota :" + calculaCuota()+"\n";
+   
+    }
     
 }
