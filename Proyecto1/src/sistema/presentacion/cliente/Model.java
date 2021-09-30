27695bc57607842449e636ea5009f33735c9f588
Elias29-6 <80367751+Elias29-6@users.noreply.github.com>
2021-09-29 18:14:17 -0600

    v0.5

---
+++ Proyecto1/src/sistema/presentacion/cliente/Model.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.presentacion.cliente;
+
+/**
+ *
+ * @author 50663
+ */
+public class Model {
+    
+}
