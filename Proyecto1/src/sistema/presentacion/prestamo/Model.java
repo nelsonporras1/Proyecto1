87e313a5e71e731a0d03b7160f9486a06989f228
nelsonporras1 <80367737+nelsonporras1@users.noreply.github.com>
2021-10-03 16:05:17 -0600

    presentacion.prestamo
    presentacion.prestamo

---
+++ Proyecto1/src/sistema/presentacion/prestamo/Model.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistema.presentacion.prestamo;
+
+
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import sistema.logic.Cliente;
+import sistema.logic.Prestamo;
+
+public class Model extends Observable{
+    
+    // Model attributes here
+    // Model gets and sets here
+    
+    Prestamo prestamo;
+    List<Prestamo> prestamos;
+    List<Cliente> clientes;
+
+    public Factura getFactura() {
+        return factura;
+    }
+
+    public void setFactura(Factura factura) {
+        this.factura = factura;
+    }
+
+    public List<Factura> getFacturas() {
+        return facturas;
+    }
+
+    public void setFacturas(List<Factura> facturas) {
+        this.facturas = facturas;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    
+    @Override
+    public synchronized void addObserver(Observer o) {
+        super.addObserver(o); 
+        this.commit();
+    }
+    
+    public void commit(){
+        this.setChanged();
+        this.notifyObservers();
+    }
+    
+}
+
+
